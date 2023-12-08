e14776ecc7d91c9f67ac27aa1ab6ee9a42dcd972
Vlad Prudius <vladvlpr006@gmail.com>
2023-12-08 12:25:44 +0200

    Added artists controller

---
+++ MusicCatalog.WebAPI/Dtos/Artists/CreateArtistDto.cs
@@ -0,0 +1,11 @@
+﻿using MusicCatalog.Domain.Models;
+
+namespace MusicCatalog.WebAPI.Dtos.Artists;
+
+public class CreateArtistDto
+{
+    public required string FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public ICollection<Song>? Songs { get; set; } = new List<Song>();
+}
---
+++ MusicCatalog.WebAPI/Dtos/Artists/UpdateArtistDto.cs
@@ -0,0 +1,12 @@
+﻿using MusicCatalog.Domain.Models;
+
+namespace MusicCatalog.WebAPI.Dtos.Artists;
+
+public class UpdateArtistDto
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public DateTime DateOfBirth { get; set; }
+    public ICollection<Song> Songs { get; set; } = null!;
+}
